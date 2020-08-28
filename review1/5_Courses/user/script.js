0b6f69a29432e88e84f81b2bcb23b1efc38bd245
Ashwin <acchu99@gmail.com>
2020-08-28 13:45:00 +0530

    Add files via upload

---
+++ review1/5_Courses/user/script.js
@@ -0,0 +1,29 @@
+// course view for users
+
+resources = [
+    {
+        "name": "Resource 1",
+        "url": "",
+        "desc": "Description goes here",
+    },
+    {
+        "name": "Resource 2",
+        "url": "",
+        "desc": "Description goes here",
+    },
+    {
+        "name": "Resource 3",
+        "url": "",
+        "desc": "Description goes here",
+    },
+    {
+        "name": "Resource 4",
+        "url": "",
+        "desc": "Description goes here",
+    },
+    {
+        "name": "Resource 5",
+        "url": "",
+        "desc": "Description goes here",
+    },
+]
