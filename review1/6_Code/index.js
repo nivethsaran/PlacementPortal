b2274ff7c50def2377d0b9dc8820ad8c9436e0fe
Niveth_Saran <nivethsaran@gmail.com>
2020-09-18 01:06:37 +0530

    Update Code and Quiz Module

--- review1/6_Code/index.js
+++ review1/6_Code/index.js
@@ -91,3 +91,19 @@ function clearCode()
 {
     editor.setValue("//Enter Code here.....")
 }
+
+var arr = ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
+"ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea co",
+"mmodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteu",
+"r sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."]
+
+function displayQuestion(i)
+{
+    document.getElementById("question").innerText = "Question" + i +": "+arr[i]
+    document.getElementById("question").hidden=false
+}
+
+function hideQuestion(i)
+{
+    document.getElementById("question").hidden = true
+}
