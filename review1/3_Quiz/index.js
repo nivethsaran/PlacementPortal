709c3dcf383be9ce32c3349aa51dc277ec463880
Niveth_Saran <nivethsaran@gmail.com>
2020-09-18 00:57:43 +0530

    Update Code and Quiz Module

--- review1/3_Quiz/index.js
+++ review1/3_Quiz/index.js
@@ -26,11 +26,29 @@ function togglePin()
     }
     else{
         document.getElementById("pin").disabled = true;
+        document.getElementById("pin-label").innerText = "Pin"
+        document.getElementById("pin-label").style.color = "black"
+    }
+}
+
+function validpin()
+{
+    var pin= document.getElementById("pin").value
+    if(pin=="" || pin.length!=4)
+    {   
+        document.getElementById("pin-label").innerText="Pin (Invalid)"
+        document.getElementById( "pin-label").style.color = "red"
+    }
+    else{
+        document.getElementById("pin-label").innerText = "Pin"
+        document.getElementById("pin-label").style.color = "black"
     }
 }
 
 function changeNOOQ()
 {
+
+    
 
     let count=parseInt(document.getElementById("nooq").value)
 
@@ -39,26 +57,30 @@ function changeNOOQ()
     {
         cardlistinnerhtml += `<div class=\"card\">\n
             <div class=\"cardData\">\n
-                <label for=\"exampleInputPassword1\">Question `+i+`</label>\n
-                <input type=\"text\\" class=\"form-control\" id=\"question`+ i +`\">\n
+                <label for=\"question`+ i + `\" id=\"question`+ i + `-label\">Question `+i+`</label>\n
+                <input type=\"text\\" class=\"form-control\" id=\"question`+ i + `\"  onclick = \"questionvalid(` + i + `)\" onkeyup = \"questionvalid(` + i + `)\" >\n
             <br >\n
             <div class=\"form-row\">\n
             <div class=\"input-group col-md-2\">\n
-            <input type =\"text\" class=\"form-control\" id=\"opt`+ i +`a\" placeholder=\"option a\">\n
+            <label for=\"opt`+ i + `a\" id=\"opt`+ i + `a-label\"></label>\n
+            <input type =\"text\" class=\"form-control\" id=\"opt`+ i + `a\" placeholder=\"option a\" onclick = \"optionavalid(` + i + `)\" onkeyup = \"optionavalid(` + i + `)\">\n
                           </div >\n
             <div class=\"input-group col-md-2\">\n
-            <input type =\"text\" class=\"form-control\" id=\"opt`+ i +`b\" placeholder=\"option b\">\n
+            <label for=\"opt`+ i + `b\" id=\"opt`+ i + `b-label\"></label>\n
+            <input type =\"text\" class=\"form-control\" id=\"opt`+ i + `b\" placeholder=\"option b\" onclick = \"optionbvalid(` + i + `)\" onkeyup = \"optionbvalid(` + i + `)\">\n
                           </div >\n
             <div class=\"input-group col-md-2\">\n
-            <input type =\"text\" class=\"form-control\" id=\"opt`+ i +`c\" placeholder=\"option c\">\n
+            <label for=\"opt`+ i + `c\" id=\"opt`+ i + `c-label\"></label>\n
+            <input type =\"text\" class=\"form-control\" id=\"opt`+ i + `c\" placeholder=\"option c\" onclick = \"optioncvalid(` + i + `)\" onkeyup = \"optioncvalid(` + i + `)\">\n
                           </div >\n
             <div class=\"input-group col-md-2\">\n
-            <input type =\"text\" class=\"form-control\" id=\"opt`+ i +`d\" placeholder=\"option d\">\n
+            <label for=\"opt`+ i + `d\" id=\"opt`+ i + `d-label\"></label>\n
+            <input type =\"text\" class=\"form-control\" id=\"opt`+ i + `d\" placeholder=\"option d\" onclick = \"optiondvalid(` + i + `)\" onkeyup = \"optiondvalid(` + i + `)\">\n
                           </div >\n
             <div class=\"form-group col-md-4\">\n
             <select id =\"correct`+ i +`\" class=\"form-control\">\n
-            <option selected > Correct Option</option >\n
-            <option value =\"a\">a</option>\n
+            <option  disabled> Correct Option</option >\n
+            <option selected value =\"a\">a</option>\n
             <option value =\"b\">b</option>\n
             <option value =\"c\">c</option>\n
             <option value =\"d\">d</option>\n
@@ -89,7 +111,196 @@ function loadIndex()
     document.getElementById('quizlistmain').innerHTML=quizlistinnerhtml
 }
 
-function quizonclick(a)
+
+function validatenull(idname)
 {
-    alert(a);
+    var val=document.getElementById(idname).value
+    if(val==''||val==undefined)
+    {
+        if(idname=="quizname")
+        {
+            document.getElementById(idname + "-label").innerText = "Quiz Name (*Required)"
+        }
+        else{
+            document.getElementById(idname + "-label").innerText = "Quiz Description (*Required)"
+        }
+        document.getElementById(idname+"-label").style.color="red"
+    }
+    else {
+        if (idname == "quizname") {
+            document.getElementById(idname + "-label").innerText = "Quiz Name"
+        }
+        else {
+            document.getElementById(idname + "-label").innerText = "Quiz Description"
+        }
+        document.getElementById(idname + "-label").style.color = "black"
+    }
+}
+
+function validdate(){
+    var GivenDate = document.getElementById("date").value;
+    var CurrentDate = new Date();
+    GivenDate = new Date(GivenDate);
+
+    if (GivenDate < CurrentDate) {
+        document.getElementById("date-label").innerText="Date of Exam(Invalid)";
+        document.getElementById("date-label").style.color="red"
+    } else {
+        document.getElementById("date-label").innerText = "Date of Exam";
+        document.getElementById("date-label").style.color = "black"
+    }
+}
+
+function validtime()
+{
+    starttime=document.getElementById("starttime").value
+    endtime=document.getElementById("endtime").value
+    if(starttime!="" && endtime!="")
+    {
+        sarr = starttime.split(":")
+        earr = endtime.split(":")
+        sh=parseInt(sarr[0])
+        sm=parseInt(sarr[1])
+        eh=parseInt(earr[0])
+        em=parseInt(earr[1])
+        console.log(sh+" "+sm+" "+eh+" "+em);
+        if(sh>eh)
+        {
+            document.getElementById("starttime-label").innerText="Start Window(Invalid)"
+            document.getElementById("endtime-label").innerText = "End Window(Invalid)"
+            document.getElementById("starttime-label").style.color = "red"
+            document.getElementById("endtime-label").style.color = "red"
+        }
+        else if(sh==eh && sm>em)
+        {
+            document.getElementById("starttime-label").innerText = "Start Window(Invalid)"
+            document.getElementById("endtime-label").innerText = "End Window(Invalid)"
+            document.getElementById("starttime-label").style.color = "red"
+            document.getElementById("endtime-label").style.color = "red"
+        }
+        else{
+            document.getElementById("starttime-label").innerText = "Start Window"
+            document.getElementById("endtime-label").innerText = "End Window"
+            document.getElementById("starttime-label").style.color = "black"
+            document.getElementById("endtime-label").style.color = "black"
+        }
+
+    }
+    else{
+    }
+
+}
+
+function validdur()
+{
+    duration=document.getElementById("duration").value
+    starttime = document.getElementById("starttime").value
+    endtime = document.getElementById("endtime").value
+    if (starttime != "" && endtime != "") {
+        sarr = starttime.split(":")
+        earr = endtime.split(":")
+        sh = parseInt(sarr[0])
+        sm = parseInt(sarr[1])
+        eh = parseInt(earr[0])
+        em = parseInt(earr[1])
+        dur=(eh-sh)*60+(em-sm)
+        if(duration!="")
+        {
+            if (parseInt(duration) > dur) {
+                document.getElementById("duration-label").innerText = "Duration (Enter Valid Data)"
+                document.getElementById("duration-label").style.color = "red"
+            }
+            else {
+
+                document.getElementById("duration-label").innerText = "Duration (in minutes)"
+                document.getElementById("duration-label").style.color = "black"
+            }
+        }
+        else{
+            document.getElementById("duration-label").innerText = "Duration (Enter Valid Data)"
+            document.getElementById("duration-label").style.color = "red"
+        }
+    }
+    else{
+        document.getElementById("starttime-label").innerText = "Start Window(Fill these)"
+        document.getElementById("endtime-label").innerText = "End Window(Fill these)"
+        document.getElementById("starttime-label").style.color = "red"
+        document.getElementById("endtime-label").style.color = "red"
+    }
+}
+
+function questionvalid(i)
+{
+    
+    var question=document.getElementById("question"+i).value
+    console.log(question)
+    if(question=="")
+    {
+        document.getElementById("question"+i+"-label").innerText = "Question"+i+"(Invalid)";
+        document.getElementById("question"+i+"-label").style.color = "red"
+    }
+    else
+    {
+        document.getElementById("question"+i+"-label").innerText = "Question" + i;
+        document.getElementById("question"+i+"-label").style.color = "black"
+    }
+}
+
+function optionavalid(i) {
+
+    var question = document.getElementById("opt" + i+"a").value
+    console.log(question)
+    if (question == "") {
+        document.getElementById("opt" + i + "a" + "-label").innerText = "Invalid Option";
+        document.getElementById("opt" + i + "a" + "-label").style.color = "red"
+    }
+    else {
+        document.getElementById("opt" + i + "a"+ "-label").innerText = "";
+        document.getElementById("opt" + i + "a" + "-label").style.color = "black"
+    }
+}
+function optionbvalid(i) {
+
+    var question = document.getElementById("opt" + i + "b").value
+    console.log(question)
+    if (question == "") {
+        document.getElementById("opt" + i + "b" + "-label").innerText = "Invalid Option";
+        document.getElementById("opt" + i + "b" + "-label").style.color = "red"
+    }
+    else {
+        document.getElementById("opt" + i + "b" + "-label").innerText = "";
+        document.getElementById("opt" + i + "b" + "-label").style.color = "black"
+    }
+}
+function optioncvalid(i) {
+
+    var question = document.getElementById("opt" + i + "c").value
+    console.log(question)
+    if (question == "") {
+        document.getElementById("opt" + i + "c" + "-label").innerText = "Invalid Option";
+        document.getElementById("opt" + i + "c" + "-label").style.color = "red"
+    }
+    else {
+        document.getElementById("opt" + i + "c" + "-label").innerText = "";
+        document.getElementById("opt" + i + "c" + "-label").style.color = "black"
+    }
+}
+
+function optiondvalid(i) {
+
+    var question = document.getElementById("opt" + i + "d").value
+    console.log(question)
+    if (question == "") {
+        document.getElementById("opt" + i + "d" + "-label").innerText = "Invalid Option";
+        document.getElementById("opt" + i + "d" + "-label").style.color = "red"
+    }
+    else {
+        document.getElementById("opt" + i + "d" + "-label").innerText = "";
+        document.getElementById("opt" + i + "d" + "-label").style.color = "black"
+    }
+}
+
+
+function quizonclick(a) {
+    // alert(a);
 }
--- review1/6_Code/index.js
+++ review1/6_Code/index.js
@@ -2,7 +2,7 @@ var config, editor;
 
 config = {
     lineNumbers: true,
-    mode: "python",
+    mode: "text/x-csrc",
     theme: "material",
     indentWithTabs: true,
     // readOnly: true
@@ -23,4 +23,71 @@ editor = CodeMirror.fromTextArea(document.getElementById("code"), config);
 
 function showCode() {
     console.log(editor.getValue().split("\n"));
+}
+
+function changeCode(lang)
+{
+    var mode="text/x-csrc"
+    console.log(lang);
+    if(lang=="C++")
+    {
+        
+        mode ="text/x-c++src"   
+    }
+    else if(lang=="JAVA")
+    {
+        mode ="text/x-java"
+    }
+    else if(lang=="PYTHON")
+    {
+        mode ="text/x-python"
+    }
+    else if(lang=="C")
+    {
+        mode ="text/x-csrc"
+    }
+    document.getElementById("language-label").innerText = "Language (" + lang + ")"
+    editor.setOption("mode",mode)
+}
+
+function validateinput()
+{
+    var cinput=document.getElementById("custominput").value
+    if(cinput=="")
+    {
+        document.getElementById("custominput-label").innerText="Custom Input (Invalid)"
+        document.getElementById("custominput-label").style.color="red"
+    }
+    else{
+        document.getElementById("custominput-label").innerText = "Custom Input"
+        document.getElementById("custominput-label").style.color = "black"
+    }
+}
+
+function toggleinput()
+{
+    var cb=document.getElementById("custominput-check")
+    if(cb.checked)
+    {
+        document.getElementById("custominput").disabled=false
+    }
+    else {
+        document.getElementById("custominput").disabled = true
+        document.getElementById("custominput-label").innerText = "Custom Input"
+        document.getElementById("custominput-label").style.color = "black"
+    }
+}
+
+function copyCode()
+{
+    navigator.clipboard.writeText(editor.getValue()).then(function () {
+        console.log('Async: Copying to clipboard was successful!');
+    }, function (err) {
+        console.error('Async: Could not copy text: ', err);
+    });
+}
+
+function clearCode()
+{
+    editor.setValue("//Enter Code here.....")
 }
